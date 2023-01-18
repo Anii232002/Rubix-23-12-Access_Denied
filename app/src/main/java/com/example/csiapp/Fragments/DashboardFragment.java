a2f639092d6c65ac8b053fa49067baa49a5f385b
Rajkamal <rajkamal.singh1011@gmail.com>
2023-01-18 14:13:19 +0530

    improv: Added Fragment categories.

---
+++ app/src/main/java/com/example/csiapp/Fragments/DashboardFragment.java
@@ -0,0 +1,65 @@
+package com.example.csiapp.Fragments;
+
+import android.graphics.Color;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.csiapp.R;
+import com.example.csiapp.databinding.FragmentDashboardBinding;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+
+import java.util.ArrayList;
+
+public class DashboardFragment extends Fragment {
+
+
+    public DashboardFragment() {
+        // Required empty public constructor
+    }
+
+    FragmentDashboardBinding binding;
+    PieChart pieChart;
+    int[] colorArr = new int[] {Color.LTGRAY,Color.BLUE,Color.CYAN,Color.DKGRAY,Color.GREEN,Color.MAGENTA,Color.RED};
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        binding = FragmentDashboardBinding.inflate(getLayoutInflater());
+
+        pieChart = binding.piechart;
+
+        PieDataSet pieDataSet = new PieDataSet(dataValuesPC(),"");
+        pieDataSet.setColors(colorArr);
+
+        PieData pieData = new PieData(pieDataSet);
+        pieChart.setData(pieData);
+
+        return binding.getRoot();
+    }
+
+    private ArrayList<PieEntry> dataValuesPC(){
+        ArrayList<PieEntry> dataVals = new ArrayList<>();
+        dataVals.add(new PieEntry(15,"Linux"));
+        dataVals.add(new PieEntry(34,"Html"));
+        dataVals.add(new PieEntry(23,"Docker"));
+        dataVals.add(new PieEntry(86,"DevOps"));
+        dataVals.add(new PieEntry(26,"Kubernetes"));
+        dataVals.add(new PieEntry(17,"Laravel"));
+        dataVals.add(new PieEntry(63,"SQL"));
+        return dataVals;
+    }
+
+}
--- app/src/main/java/com/example/csiapp/MainActivity.kt
+++ app/src/main/java/com/example/csiapp/MainActivity.kt
@@ -3,6 +3,10 @@ package com.example.csiapp
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.example.csiapp.Fragments.BlogsFragment
+import com.example.csiapp.Fragments.ChatBotFragment
+import com.example.csiapp.Fragments.DashboardFragment
+import com.example.csiapp.Fragments.DiaryFragment
 import me.ibrahimsn.lib.OnItemSelectedListener
 import me.ibrahimsn.lib.SmoothBottomBar
 
@@ -12,23 +16,24 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
+        supportFragmentManager.beginTransaction().replace(R.id.fragment_container, DashboardFragment())
+            .commit()
+
+        val sf = getSharedPreferences("ShredPref", MODE_PRIVATE)
         val bottomNavigationView = findViewById<View>(R.id.bottom_navigation) as SmoothBottomBar
         bottomNavigationView.onItemSelectedListener = object : OnItemSelectedListener {
             override fun onItemSelect(i: Int): Boolean {
                 when (i) {
-
-                    // For bottom navigaton
-
-//                    0 -> supportFragmentManager.beginTransaction()
-//                        .replace(R.id.fragment_container, Study_Fragment()).commit()
-//                    1 -> supportFragmentManager.beginTransaction()
-//                        .replace(R.id.fragment_container, Analysis_Fragment()).commit()
-//                    3 -> supportFragmentManager.beginTransaction()
-//                        .replace(R.id.fragment_container, Profile_Fragment()).commit()
-//                    2 -> supportFragmentManager.beginTransaction()
-//                        .replace(R.id.fragment_container, Test_Fragment()).commit()
-//                    else -> supportFragmentManager.beginTransaction()
-//                        .replace(R.id.fragment_container, Study_Fragment()).commit()
+                    0 -> supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container, DashboardFragment()).commit()
+                    1 -> supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container, DiaryFragment()).commit()
+                    3 -> supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container, ChatBotFragment()).commit()
+                    2 -> supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container, BlogsFragment()).commit()
+                    else -> supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container, DashboardFragment()).commit()
                 }
                 return true
             }
