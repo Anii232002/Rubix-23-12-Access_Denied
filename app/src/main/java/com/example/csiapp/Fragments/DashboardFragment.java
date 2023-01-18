d03ba0bc0b09d712a928d882ad86e28ef7bed518
Rajkamal <rajkamal.singh1011@gmail.com>
2023-01-18 16:50:18 +0530

    Added calorie tracker UI in DashboardFragment and created ExerciseActivity.

--- app/src/main/java/com/example/csiapp/Fragments/DashboardFragment.java
+++ app/src/main/java/com/example/csiapp/Fragments/DashboardFragment.java
@@ -1,5 +1,6 @@
 package com.example.csiapp.Fragments;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 
@@ -9,15 +10,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.example.csiapp.R;
+import com.example.csiapp.ExerciseActivity;
 import com.example.csiapp.databinding.FragmentDashboardBinding;
 import com.github.mikephil.charting.charts.PieChart;
-import com.github.mikephil.charting.data.BarData;
-import com.github.mikephil.charting.data.BarDataSet;
-import com.github.mikephil.charting.data.BarEntry;
-import com.github.mikephil.charting.data.Entry;
-import com.github.mikephil.charting.data.PieData;
-import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
 
 import java.util.ArrayList;
@@ -32,6 +27,8 @@ public class DashboardFragment extends Fragment {
     FragmentDashboardBinding binding;
     PieChart pieChart;
     int[] colorArr = new int[] {Color.LTGRAY,Color.BLUE,Color.CYAN,Color.DKGRAY,Color.GREEN,Color.MAGENTA,Color.RED};
+    static int maxCal = 2300;
+    static int currCal = 1200;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -39,13 +36,27 @@ public class DashboardFragment extends Fragment {
         // Inflate the layout for this fragment
         binding = FragmentDashboardBinding.inflate(getLayoutInflater());
 
-        pieChart = binding.piechart;
+        /*pieChart = binding.piechart;
 
         PieDataSet pieDataSet = new PieDataSet(dataValuesPC(),"");
         pieDataSet.setColors(colorArr);
 
+        pieChart.setDrawEntryLabels(false);
         PieData pieData = new PieData(pieDataSet);
-        pieChart.setData(pieData);
+        pieChart.setData(pieData);*/
+
+        binding.speedView.setMaxSpeed(maxCal);
+        binding.speedView.speedTo(currCal, 1000);
+        binding.speedView.setWithTremble(false);
+
+
+        binding.exercise.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getContext(), ExerciseActivity.class));
+            }
+        });
+
 
         return binding.getRoot();
     }
