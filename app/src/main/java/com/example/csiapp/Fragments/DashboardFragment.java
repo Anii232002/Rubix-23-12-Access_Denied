4aadef23721f6632045a2ff8ee59aabb1e9702cc
Umang <umang2singh@gmail.com>
2023-01-19 20:16:48 +0530

    Completed streak

--- app/src/main/java/com/example/csiapp/Fragments/DashboardFragment.java
+++ app/src/main/java/com/example/csiapp/Fragments/DashboardFragment.java
@@ -1,17 +1,24 @@
 package com.example.csiapp.Fragments;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 
+import com.example.csiapp.R;
 import com.example.csiapp.UserInfo;
 import com.example.csiapp.databinding.FragmentDashboardBinding;
 import com.github.mikephil.charting.charts.PieChart;
@@ -28,7 +35,9 @@ import com.google.android.material.floatingactionbutton.ExtendedFloatingActionBu
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.firestore.auth.User;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Calendar;
 
 public class DashboardFragment extends Fragment {
 
@@ -44,11 +53,29 @@ public class DashboardFragment extends Fragment {
     static int currCal = 1200;
     private FrameLayout fb ;
 
+    private TextView streakNumber;
+    private ImageView streakImg;
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        streakNumber=binding.streakNumber;
+        streakImg=binding.streakImg;
+
+        int streak = updateStreak();
+
+        streakNumber.setText(String.valueOf(streak));
+
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         binding = FragmentDashboardBinding.inflate(getLayoutInflater());
+
+
 
         pieChart = binding.piechart;
 
@@ -84,6 +111,63 @@ public class DashboardFragment extends Fragment {
 
         return binding.getRoot();
     }
+
+    private int updateStreak() {
+
+        String today=null;
+        String dateOnSp;
+        String expectedYesterday = null;
+        int storedStreak;
+        LocalDate t = null,y=null;
+
+        SharedPreferences preferences = this.getActivity().getSharedPreferences("StreakPref", Context.MODE_PRIVATE);
+        dateOnSp = preferences.getString("date","");
+        storedStreak = preferences.getInt("streak",0);
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            t = LocalDate.now();
+            today=t.toString();
+        }
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            y = t.minusDays(1);
+            expectedYesterday=y.toString();
+        }
+
+        // already updated today
+        if(dateOnSp.equals(today)){
+            return storedStreak;
+        }
+
+        // visiting for the first time
+        if(dateOnSp.equals("")){
+            SharedPreferences.Editor editor = preferences.edit();
+            editor.putString("date",today);
+            editor.putInt("streak",0);
+            editor.apply();
+            streakImg.setImageResource(R.drawable.nonfire);
+            return 0;
+        }
+
+        // came yesterday
+        if(dateOnSp.equals(expectedYesterday)){
+            SharedPreferences.Editor editor = preferences.edit();
+            editor.putString("date",today);
+            editor.putInt("streak",storedStreak+1);
+            editor.apply();
+            streakImg.setImageResource(R.drawable.fire);
+            return storedStreak+1;
+        }
+
+        //visiting app after long time
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString("date",today);
+        editor.putInt("streak",0);
+        editor.apply();
+        streakImg.setImageResource(R.drawable.nonfire);
+        return 0;
+
+    }
+
 
     private ArrayList<PieEntry> dataValuesPC(){
         ArrayList<PieEntry> dataVals = new ArrayList<>();
